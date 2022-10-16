79fafa0ad139966b3a68e5dfacee4ec3f95df8f2
SH-Won <1317good@naver.com>
2022-10-16 18:50:19 +0900

    메인 주석 변경 2

--- client/src/main.js
+++ client/src/main.js
@@ -11,6 +11,7 @@ Vue.directive("lazyload", LazyImageLoad);
 // git pull request test
 // git pull request test 2
 // git pull request test 3
+// git pull request test 4
 new Vue({
   router,
   store,
