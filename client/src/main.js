47a9b2534f7bb7203730d929f454f183f4497edf
SH-Won <1317good@naver.com>
2022-10-23 18:53:26 +0900

    #6 add Token

--- client/src/main.js
+++ client/src/main.js
@@ -8,6 +8,29 @@ Vue.config.productionTip = false;
 Vue.directive("lazyload", LazyImageLoad);
 
 // main 시작점
+
+// git pull request test
+// git pull request test 2
+// git pull request test 3
+// git pull request test 4
+
+const hotfix = {
+  name : 'hotfix/#6_AddToken221023_master',
+  time : '06:28'
+}
+const hotfix1 = {
+  name : 'hotfix/#6_AddToken221023_master_v1',
+  time : '06:29',
+}
+const hotfix2 = {
+  name : 'hotfix/#6_AddToken221023_master',
+  time : '06:28'
+}
+const hotfix3 = {
+  name : 'hotfix/#6_AddToken221023_master_v1',
+  time : '06:29',
+}
+
 new Vue({
   router,
   store,
